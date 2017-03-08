9b2f66ae82e9db7f5a6dd5cb8d8c0c06bd7de37e
pangjuntaoer <pangjuntaoer@163.com>
2017-03-08 09:38:13 +0800

    ts
    sd

---
+++ LiveBaseTestNg.java
@@ -0,0 +1,14 @@
+package com.netease.edu.live.test;
+
+import org.springframework.test.context.ContextConfiguration;
+
+import com.netease.edu.utest.BaseTestNg;
+
+/**
+ * 
+ * @author Doraemon
+ *
+ */
+@ContextConfiguration(locations = {"classpath:/applicationContext-scratch.xml"})
+public class LiveBaseTestNg extends BaseTestNg{
+}
